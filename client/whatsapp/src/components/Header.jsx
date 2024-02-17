459ddc646df160a10b163d0a23eb7fddd4b9663a
abhayrsr <abhayrajsinghrana123@gmail.com>
2024-02-18 00:57:50 +0530

    profile bar

--- client/whatsapp/src/components/Header.jsx
+++ client/whatsapp/src/components/Header.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
-import { useContext } from 'react';
-import { AccountContext } from '../context/AccountProvider';
+import React from "react";
+import { useContext } from "react";
+import { AccountContext } from "../context/AccountProvider";
+import { Box, styled } from "@mui/material";
+import { Chat as MessageIcon, MoreVert } from "@material-ui/icons";
+
+const Component = styled(Box)`
+  height: 44px;
+  background: #ededed;
+  padding: 8px 16px;
+  display: flex;
+  align-items: centre;
+`;
+
+const Wrapper = styled(Box)`
+  margin-left: auto;
+  & > * {
+    margin-left: 2px;
+    padding: 8px;
+    color: #000;
+  }
+
+  &: first-child {
+    font-size: 22px;
+    margin-right: 8px;
+    margin-top: 3px;
+  }
+`;
+
+const Image = styled("img")({
+  height: 40,
+  width: 40,
+  borderRadius: "50%",
+});
 
 export default function Header() {
+  const { account } = useContext(AccountContext);
   return (
-    <div>Header</div>
-  )
+    <>
+      <Component>
+        <Image src={account.picture} alt="dp" />
+        <Wrapper>
+          <MessageIcon />
+          <MoreVert />
+        </Wrapper>
+      </Component>
+    </>
+  );
 }
